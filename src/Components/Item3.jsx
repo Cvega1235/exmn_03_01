b6f1bc46cad5aea55f5a24d17f74b6504c10ecf6
root <kikivega2002@gmail.com>
2023-11-30 11:27:02 -0400

    Componente Item3 realizado

---
+++ src/Components/Item3.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Item.css'
+function Item3() {
+  return (
+    <div className='cont'>
+        <img src="/image (2).svg" alt="" />
+        <div className='text'>
+            <h4>
+            Remada unilateral
+            </h4>
+            <p>3 séries x 12 repetições</p>
+        </div>
+        <img src="/Vector (Stroke).svg" alt="" />
+    </div>
+  )
+}
+
+export default Item3
