fb22fea9f0b501580327aeaff3d60d63aef570a1
root <kikivega2002@gmail.com>
2023-11-30 11:26:31 -0400

    Componente Item2 realizado

---
+++ src/Components/Item2.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Item.css'
+function Item2() {
+  return (
+    <div className='cont'>
+        <img src="/image (1).svg" alt="" />
+        <div className='text'>
+            <h4>
+            Remada curvada
+            </h4>
+            <p>3 séries x 12 repetições</p>
+        </div>
+        <img src="/Vector (Stroke).svg" alt="" />
+    </div>
+  )
+}
+
+export default Item2
