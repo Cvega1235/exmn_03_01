be553a7c82081614a94a567783d829ec60ada1a7
root <kikivega2002@gmail.com>
2023-11-30 10:58:14 -0400

    Componente info realizado

---
+++ src/Components/Informa.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Informa.css'
+function Informa() {
+  return (
+    <div className='con'>
+      <h3>Exercícios</h3>
+      <h3>4</h3>
+    </div>
+  )
+}
+
+export default Informa
