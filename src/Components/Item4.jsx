2590c14b2de8ab628fa275c53998d2181a9662be
root <kikivega2002@gmail.com>
2023-11-30 11:27:27 -0400

    Componente Item4 realizado

---
+++ src/Components/Item4.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Item.css'
+function Item4() {
+  return (
+    <div className='cont'>
+        <img src="/image (3).svg" alt="" />
+        <div className='text'>
+            <h4>
+            Levantamento terra
+            </h4>
+            <p>3 séries x 12 repetições</p>
+        </div>
+        <img src="/Vector (Stroke).svg" alt="" />
+    </div>
+  )
+}
+
+export default Item4
